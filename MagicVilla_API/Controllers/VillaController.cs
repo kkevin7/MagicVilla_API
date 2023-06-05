b4bf3da00f1eed3d1063c65e92af715a23d661ca
Kevin Martinez <kkevinmartinez7@gmail.com>
2023-06-04 20:34:41 -0600

    First Endpoint

---
+++ MagicVilla_API/Controllers/VillaController.cs
@@ -0,0 +1,21 @@
+﻿using MagicVilla_API.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MagicVilla_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class VillaController : ControllerBase
+    {
+        [HttpGet]
+        public IEnumerable<Villa> GetVillas()
+        {
+            return new List<Villa>
+            {
+                new Villa{Id=1, Nombre="Vista a la Piscina"},
+                new Villa{Id=2, Nombre="Vista a la Playa"},
+            };
+        }
+    }
+}
